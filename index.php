13c298c5a0a2cb1230868d4f6dc2b61fe3f98476
makubex64 <jorgeluismakubex@gmail.com>
2020-08-13 13:31:45 -0400

    add files

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php get_header(); ?>
 
     <!-- entradas -->
-    <div class="container mt-5 img-fluid">
 
-    <div class="row img-fluid">
-    <div class="col-lg-9 mt-5 img-fluid">
+    <div class="row img-fluid mt-5">
+    <div class="col-lg-7 mt-5 img-fluid">
 
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-        <div class="entradas card mb-3 shadow p-3 mb-5 bg-white rounded" style="width: 35rem;">
+        <div class="entradas card mb-3 shadow p-3 mb-5 bg-white rounded" style="
+        width: 32rem;
+        margin-left: 5px;
+        ">
             <a href="<?php the_permalink(); ?>">
             <?php if ( has_post_thumbnail() ) {
                         the_post_thumbnail('post-thumbnails', array( 'class' => 'img-fluid mb-0' ));
@@ -32,23 +34,21 @@
 
 
             </div>
-    		</div>
+            <!-- pagination -->
+            <div class="container mb-5 py-5">
+                <!-- llamar subcarpta -->
+                <div class="col-lg-11 float-right mb-5">
+                <?php get_template_part('template-parts/content', 'pagination'); ?>
+                </div>
+
+            </div>
+            <!-- end pagination -->
+
     	</div>
     </div>
 
 
-<!-- pagination -->
-<div class="container mb-5 py-5">
-    <!-- llamar subcarpta -->
-    <div class="col-lg-9 float-right mb-5">
-    <?php get_template_part('template-parts/content', 'pagination'); ?>
-    </div>
 
-</div>
-<!-- end pagination -->
-
-
-
- <?php get_footer(); ?> 
-  </body>
+<?php get_footer(); ?> 
+</body>
 </html>
--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,4 @@
-<div class="col-lg-3 mt-5">
+<div class="col-lg-5 mt-5 img-fluid">
 	<!-- <div class="card-body">
         <h4>Publicidad</h4>
         <hr>
@@ -10,6 +10,22 @@
 	    <?php else : ?>
 	        <!-- Time to add some widgets! -->
 	    <?php endif; ?>
+
+<div class="card mb-3 img-fluid" style="max-width: 540px;">
+  <div class="row no-gutters">
+    <div class="col-md-4">
+      <img src="..." class="card-img" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
     			
  </div>
+
     			
