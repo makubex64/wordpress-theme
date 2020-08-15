6ab8f3e90c05ad816eaa717bcd60f99a944683a6
Luis Jorge Adarmes Rodriguez <jorgeluismakubex@gmail.com>
2020-08-14 20:34:34 -0400

    add files

--- footer.php
+++ footer.php
@@ -1,7 +1,6 @@
-<footer class=" py-4 text-center p-5" style="
-background: #141E30;  /* fallback for old browsers */
-background: -webkit-linear-gradient(to bottom, #243B55, #141E30);  /* Chrome 10-25, Safari 5.1-6 */
-background: linear-gradient(to bottom, #243B55, #141E30); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+<footer class=" py-4 text-center p-5 bg-light text-dark" style="
+
+
 
 
 
@@ -9,15 +8,49 @@ background: linear-gradient(to bottom, #243B55, #141E30); /* W3C, IE 10+/ Edge, 
       	
 <div class="container py-5">
   <div class="row">
-    <div class="col-sm">
-     <p class="text-white">Sitio web diseñado por <a href="https://www.linkedin.com/in/jorge-adarmes-rodriguez-98156112a/">Jorge Adarmes</a> gestionado con <a href=""><i class="fab fa-wordpress"></i> Wordpress</a> 2019-2020</p>
+    <div class="col-lg-12" style="
+
+  .fa {
+  padding: 20px;
+  font-size: 30px;
+  width: 30px;
+  text-align: center;
+  text-decoration: none;
+  margin: 5px 2px;
+  border-radius: 50%;
+  }
+
+  .fa:hover {
+    opacity: 0.7;
+	}
+
+.fa-facebook {
+  background: #3B5998;
+  color: white;
+}
+
+.fa-twitter {
+  background: #55ACEE;
+  color: white;
+}
+.fa-linkedin {
+  background: #007bb5;
+  color: white;
+}
+
+    ">
+     <p class="text-dark">Sitio web diseñado por <a href="https://www.linkedin.com/in/jorge-adarmes-rodriguez-98156112a/" target="_blank">Jorge Adarmes</a> gestionado con <a href="https://wordpress.org/" target="_blank"><i class="fab fa-wordpress"></i> Wordpress</a> 2019-2020</p>
+
+    
+     <a href="https://www.facebook.com/jorge.23anmx" class="fa fa-facebook" target="_blank"></a>
+	 <a href="https://twitter.com/makubex64" class="fa fa-twitter" target="_blank"></a>
+	 <a href="https://www.linkedin.com/in/jorge-adarmes-rodriguez-98156112a/" class="fa fa-linkedin" target="_blank"></a>
+
     </div>
     <div class="col-sm">
      
     </div>
-    <div class="col-sm">
-     <p class="text-muted">Sitio web diseñado por <a href="https://www.linkedin.com/in/jorge-adarmes-rodriguez-98156112a/">Jorge Adarmes</a> gestionado con <a href=""><i class="fab fa-wordpress"></i> Wordpress</a> 2019-2020</p>
-    </div>
+
   </div>
 </div>
 
